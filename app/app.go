d7ed8327e1e4e19cf591a626d8c0179ce81c4223
Twelfthonehone <95571424+honehone12@users.noreply.github.com>
2026-01-03 18:29:24 +0900

    added mime guard

--- app/app.go
+++ app/app.go
@@ -114,7 +114,7 @@ func (a *App) Close() error {
 }
 
 func (a *App) bind(ctx echo.Context, target any) error {
-	raw, _, _ := strings.Cut(ctx.Request().Header.Get("Content-Type"), ";")
+	raw, _, _ := strings.Cut(ctx.Request().Header.Get(echo.HeaderContentType), ";")
 	contentType := strings.TrimSpace(raw)
 	if contentType != echo.MIMEApplicationForm {
 		return errors.New("unexpected mime type")
